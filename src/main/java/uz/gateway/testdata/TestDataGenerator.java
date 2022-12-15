3aba5eb1c17912064aac1a78172cdd374f4be4b6
Bulat Maskurov <b.maskurov@uzum.com>
2022-12-15 11:41:36 +0500

    Доработка генерацию тестовых данных

--- src/main/java/uz/gateway/testdata/TestDataGenerator.java
+++ src/main/java/uz/gateway/testdata/TestDataGenerator.java
@@ -1,6 +1,8 @@
 package uz.gateway.testdata;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import uz.gateway.testdata.pojo.Client;
+import uz.gateway.testdata.pojo.Server;
 import uz.gateway.testdata.pojo.TestData;
 import uz.gateway.testdata.pojo.User;
 
@@ -13,31 +15,55 @@ import java.util.List;
  */
 public class TestDataGenerator {
 
+    static TestData testData = readTestData();
+
     /*
-     * Метод находит и возвращает пользователя по полю 'alias'
+     * Метод находит и возвращает server по полю 'alias'
      */
-    public User getUserByAlias(String alias) {
-        User testUser = getUsers().stream()
-                .filter(user -> user.getAlias().equals(alias)).findFirst().orElse(null);
-        if (testUser == null) {
-            throw new RuntimeException("Ошибка при чтении user из testdata");
+    public Server getServerByAlias(String alias) {
+        Server server = testData.getServers().stream()
+                .filter(s -> s.getAlias().equals(alias)).findFirst().orElse(null);
+        if (server == null) {
+            throw new RuntimeException("Ошибка при чтении server из testdata");
         } else {
-            return testUser;
+            return server;
         }
     }
 
-    public User getUserById(int userId) {
-        return readTestData().getUsers().get(userId);
+    /*
+     * Метод находит и возвращает client по полю 'alias'
+     */
+    public Client getClientByAlias(String alias) {
+        Client client = testData.getClients().stream()
+                .filter(c -> c.getAlias().equals(alias)).findFirst().orElse(null);
+        if (client == null) {
+            throw new RuntimeException("Ошибка при чтении client из testdata");
+        } else {
+            return client;
+        }
+    }
+
+    /*
+     * Метод находит и возвращает user по полю 'alias'
+     */
+    public User getUserByAlias(String alias) {
+        User user = getUsers().stream()
+                .filter(u -> u.getAlias().equals(alias)).findFirst().orElse(null);
+        if (user == null) {
+            throw new RuntimeException("Ошибка при чтении user из testdata");
+        } else {
+            return user;
+        }
     }
 
     public List<User> getUsers() {
-        return readTestData().getUsers();
+        return testData.getUsers();
     }
 
     /*
      * Метод читает файл resources/testdata.json и сериализует его в объект TestData
      */
-    public TestData readTestData() {
+    public static TestData readTestData() {
         ObjectMapper objectMapper = new ObjectMapper();
         TestData testData;
         try {
--- src/main/java/uz/gateway/testdata/pojo/Server.java
+++ src/main/java/uz/gateway/testdata/pojo/Server.java
@@ -1,16 +1,20 @@
 package uz.gateway.testdata.pojo;
 
-import javax.annotation.Generated;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
         "alias",
         "uri"
 })
-@Generated("jsonschema2pojo")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Server {
 
     @JsonProperty("alias")
