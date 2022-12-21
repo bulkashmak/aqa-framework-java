6e6f1fba22c4cfcbd5bcb0dcde007f25e419cd3f
Bulat Maskurov <b.maskurov@uzum.com>
2022-12-21 11:45:45 +0500

    Избавился от hardcode otp

--- src/main/java/uz/gateway/GatewayClient.java
+++ src/main/java/uz/gateway/GatewayClient.java
@@ -6,14 +6,10 @@ import io.restassured.config.RestAssuredConfig;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.stereotype.Service;
 import uz.gateway.testdata.TestDataProvider;
 
 @Service
-@Getter
-@Setter
 public class GatewayClient {
 
     public RequestSpecification defaultSpec;
