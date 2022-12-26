873ab21bad5a4ddccf296d14b4341d6ba058dc2a
Bulat Maskurov <b.maskurov@uzum.com>
2022-12-26 15:00:37 +0500

    Методы для reset password

--- src/main/java/uz/gateway/services/auth/AuthService.java
+++ src/main/java/uz/gateway/services/auth/AuthService.java
@@ -5,6 +5,9 @@ import io.restassured.response.ValidatableResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uz.gateway.GatewayClient;
+import uz.gateway.dto.auth.resetPassword.request.RequestResetPassword;
+import uz.gateway.dto.auth.resetPassword.request.RequestResetPasswordSetPassword;
+import uz.gateway.dto.auth.resetPassword.request.RequestResetPasswordVerify;
 import uz.gateway.dto.auth.signIn.request.RequestSignInVerify;
 import uz.gateway.dto.auth.signUp.request.RequestSignUp;
 import uz.gateway.dto.auth.signUp.request.RequestSignUpSetPassoword;
@@ -30,43 +33,73 @@ public class AuthService extends GatewayClient {
                 .then();
     }
 
-    public ValidatableResponse postSignInVerify(RequestSignInVerify requestSignInVerify) {
+    public ValidatableResponse postSignInVerify(RequestSignInVerify requestBody) {
         log.info("POST запрос {}", Path.SIGN_IN_VERIFY.getPath());
         return given()
                 .spec(defaultSpec)
                 .contentType(ContentType.JSON)
-                .body(requestSignInVerify)
+                .body(requestBody)
                 .post(Path.SIGN_IN_VERIFY.getPath())
                 .then();
     }
 
-    public ValidatableResponse postSignUp(RequestSignUp requestSignUp) {
+    public ValidatableResponse postSignUp(RequestSignUp requestBody) {
         log.info("POST запрос {}", Path.SIGN_UP.getPath());
         return given()
                 .spec(defaultSpec)
                 .contentType(ContentType.JSON)
-                .body(requestSignUp)
+                .body(requestBody)
                 .post(Path.SIGN_UP.getPath())
                 .then();
     }
 
-    public ValidatableResponse postSignUpVerify(RequestSignUpVerify requestSignUpVerify) {
+    public ValidatableResponse postSignUpVerify(RequestSignUpVerify requestBody) {
         log.info("POST запрос {}", Path.SIGN_UP_VERIFY.getPath());
         return given()
                 .spec(defaultSpec)
                 .contentType(ContentType.JSON)
-                .body(requestSignUpVerify)
+                .body(requestBody)
                 .post(Path.SIGN_UP_VERIFY.getPath())
                 .then();
     }
 
-    public ValidatableResponse postSignUpSetPassword(RequestSignUpSetPassoword requestSignUpSetPassoword) {
+    public ValidatableResponse postSignUpSetPassword(RequestSignUpSetPassoword requestBody) {
         log.info("POST запрос {}", Path.SIGN_UP_SET_PASSWORD.getPath());
         return given()
                 .spec(defaultSpec)
                 .contentType(ContentType.JSON)
-                .body(requestSignUpSetPassoword)
+                .body(requestBody)
                 .post(Path.SIGN_UP_SET_PASSWORD.getPath())
+                .then();
+    }
+
+    public ValidatableResponse postResetPassword(RequestResetPassword requestBody) {
+        log.info("POST запрос {}", Path.RESET_PASSWORD.getPath());
+        return given()
+                .spec(defaultSpec)
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .post(Path.RESET_PASSWORD.getPath())
+                .then();
+    }
+
+    public ValidatableResponse postResetPasswordVerify(RequestResetPasswordVerify requestBody) {
+        log.info("POST запрос {}", Path.RESET_PASSWORD_VERIFY.getPath());
+        return given()
+                .spec(defaultSpec)
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .post(Path.RESET_PASSWORD_VERIFY.getPath())
+                .then();
+    }
+
+    public ValidatableResponse postResetPasswordSetPassword(RequestResetPasswordSetPassword requestBody) {
+        log.info("POST запрос {}", Path.RESET_PASSWORD_SET_PASSWORD.getPath());
+        return given()
+                .spec(defaultSpec)
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .post(Path.RESET_PASSWORD_SET_PASSWORD.getPath())
                 .then();
     }
 
@@ -75,7 +108,10 @@ public class AuthService extends GatewayClient {
         SIGN_IN_VERIFY("/auth/sign-in/verify"),
         SIGN_UP("/auth/sign-up"),
         SIGN_UP_VERIFY("/auth/sign-up/verify"),
-        SIGN_UP_SET_PASSWORD("/auth/sign-up/set-password");
+        SIGN_UP_SET_PASSWORD("/auth/sign-up/set-password"),
+        RESET_PASSWORD("/auth/reset"),
+        RESET_PASSWORD_VERIFY("/auth/reset/verify"),
+        RESET_PASSWORD_SET_PASSWORD("/auth/reset/set-password");
 
         private final String path;
 
